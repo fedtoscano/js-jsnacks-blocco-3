09af753a4d44d56444c292c350b7a7ff309e60b9
fedtoscano <federicotoscano611@gmail.com>
2024-05-15 12:18:29 +0200

    console.log ulteriore

--- snack-10/script.js
+++ snack-10/script.js
@@ -1,11 +1,12 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-const arr1=[1,2,3,4,5]
-const arr2=[1,2,3,4,5,6,7,8,9,10]
+const arr1=[1,2,3,4,5];
+const arr2=[1,2,3,4,5,6,7,8,9,10];
 
 let longArray;
 let shortArray;
+
 if(arr1.length>arr2.length){
     longArray = arr1;
     shortArray = arr2;
@@ -16,6 +17,7 @@ if(arr1.length>arr2.length){
 
 while(shortArray.length<longArray.length){
     shortArray.push(Math.floor(Math.random()*10) +1);
-
-    console.log(shortArray.length, longArray.length)
+    console.log(shortArray.length, longArray.length);
 }
+
+console.log(shortArray, longArray);
