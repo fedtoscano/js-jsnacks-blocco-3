3fe6a184b7ab6e05a57966473a024e30adfdd5de
fedtoscano <federicotoscano611@gmail.com>
2024-05-15 12:00:42 +0200

    added ;

--- snack-9/script.js
+++ snack-9/script.js
@@ -4,10 +4,10 @@
 const nums=[];
 
 while(nums.length<6){
-    const userNum = Number.parseInt(prompt("Type a number"),10)
+    const userNum = Number.parseInt(prompt("Type a number"),10);
     if(userNum%2===1){
-        nums.push(userNum)
-        console.log(`${userNum} has ben added to the array`)
+        nums.push(userNum);
+        console.log(`${userNum} has ben added to the array`);
     }
 }
-console.log(nums)
+console.log(nums);
