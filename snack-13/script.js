dba5b8ffe432478bfa1703bdd15e1fc7657a3112
fedtoscano <federicotoscano611@gmail.com>
2024-05-16 12:24:54 +0200

    fix

--- snack-13/script.js
+++ snack-13/script.js
@@ -2,12 +2,10 @@
 
 function invertChar(num){
     let numString = num.toString();
-    // console.log(numString)
-
-    for(i=1; i<numString.length; i++){
-        const invertedNum=[]
-        invertedNum.push(numString[-i])
-        console.log(invertedNum)
+    const invertedNum= []
+    for(i=numString.length; i>=0; i--){
+        invertedNum.push(numString[i])
     }
+    console.log(invertedNum.join(""))
 
 }
