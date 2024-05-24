dad2ef50556a127ddb34ab7c8f6714b9805d031a
fedtoscano <federicotoscano611@gmail.com>
2024-05-24 11:25:08 +0200

    snack zucchine 2

--- snack-zucchine/script.js
+++ snack-zucchine/script.js
@@ -4,16 +4,16 @@
 // Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 
 const zucchine = [
-    { varieta: 'Nera di Milano', peso: 341.23, lunghezza: '14.58' },
-    { varieta: 'Romanesca', peso: 287.45, lunghezza: '19.75' },
-    { varieta: 'Fiorentina', peso: 458.12, lunghezza: '22.36' },
-    { varieta: 'Tonda', peso: 132.67, lunghezza: '25.98' },
-    { varieta: 'Lunga fiorentina', peso: 376.54, lunghezza: '28.45' },
-    { varieta: 'Striata d\'Italia', peso: 231.78, lunghezza: '11.23' },
-    { varieta: 'Bianca triestina', peso: 314.89, lunghezza: '17.56' },
-    { varieta: 'Gialla', peso: 459.31, lunghezza: '12.67' },
-    { varieta: 'Rugosa friulana', peso: 278.44, lunghezza: '26.89' },
-    { varieta: 'Patisson', peso: 387.56, lunghezza: '29.12' }
+    { varieta: 'Nera di Milano', peso: 341.23, lunghezza: 14.58 },
+    { varieta: 'Romanesca', peso: 287.45, lunghezza: 19.75 },
+    { varieta: 'Fiorentina', peso: 458.12, lunghezza: 22.36 },
+    { varieta: 'Tonda', peso: 132.67, lunghezza: 25.98 },
+    { varieta: 'Lunga fiorentina', peso: 376.54, lunghezza: 28.45 },
+    { varieta: 'Striata d\'Italia', peso: 231.78, lunghezza: 11.23 },
+    { varieta: 'Bianca triestina', peso: 314.89, lunghezza: 17.56 },
+    { varieta: 'Gialla', peso: 459.31, lunghezza: 12.67 },
+    { varieta: 'Rugosa friulana', peso: 278.44, lunghezza: 26.89 },
+    { varieta: 'Patisson', peso: 387.56, lunghezza: 29.12 }
 ]
 
 
@@ -24,3 +24,15 @@ zucchine.forEach(zucchina => {
 });
 console.log(`La somma di tutte le zucchine è: ${somma.toFixed(2)}g`)
 
+
+const zucchineAssolutamenteNellaMedia = []
+const zucchinGigantiOltremisura = []
+zucchine.map((zucchina) => {
+    if(zucchina.lunghezza < 15)
+        zucchineAssolutamenteNellaMedia.push(zucchina)
+    else
+        zucchinGigantiOltremisura.push(zucchina)
+})
+
+console.log(zucchineAssolutamenteNellaMedia)
+console.log(zucchinGigantiOltremisura)
